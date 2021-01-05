005e8628267d722d6576b1ef9b07cd304e444200
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-01-04 22:25:01 -0300

    💄 UI: Style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,60 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <img src={logoImg} alt="GithubExplorer"/>
+      <Title>Explore repositorios no Github.</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29512437?s=460&u=dc8ef0d6f9268a74b9a81eaa51ad65538d61bf54&v=4"
+            alt="Myke Oliveira"
+          />
+          <div>
+            <strong>gostack-mobile</strong>
+            <p>Aplicação desenvolvida GoStack</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29512437?s=460&u=dc8ef0d6f9268a74b9a81eaa51ad65538d61bf54&v=4"
+            alt="Myke Oliveira"
+          />
+          <div>
+            <strong>gostack-mobile</strong>
+            <p>Aplicação desenvolvida GoStack</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29512437?s=460&u=dc8ef0d6f9268a74b9a81eaa51ad65538d61bf54&v=4"
+            alt="Myke Oliveira"
+          />
+          <div>
+            <strong>gostack-mobile</strong>
+            <p>Aplicação desenvolvida GoStack</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 }
 
