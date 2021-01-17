68db8f8c78db26a148e429b4452133d4701682f2
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-01-17 13:47:35 -0300

    🚧 PROGRESS: Create page repository;

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+import { Header, RepositoryInfo, Issues } from "./styles"
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,50 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <h1>Repository:{params.repository}</h1>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer"/>
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/29512437?s=460&u=dc8ef0d6f9268a74b9a81eaa51ad65538d61bf54&v=4"
+            alt="myke-oliveira-profile"
+          />
+          <div>
+            <strong>Myke/unform</strong>
+            <p>Descrição do Repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to={"asdfasdfa"}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
